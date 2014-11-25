6d278094bc68a06f471105a837e90a2854eb7242
ifours <ifours@ifours.local>
2014-11-25 21:22:49 +0300

    main structure of main.c

--- main.c
+++ main.c
@@ -14,10 +14,47 @@ static int printResult(void *data, int argc, char **argv, char **azColName) {
 }
 
 // Functionality block
-
+int testSQL(sqlite3 *db, char *errmsg) {
+  // Build you statement here
+  return sqlite3_exec(db, "select * from users", printResult, 0, &errmsg);
+}
 // end
 
 int main(int argc, char **argv) {
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  char buffer [50];
+  int OPERATION_COUNT = 1;
+  int rc, operation, id;
 
+  // Put our operation here
+  char *states[1] = {
+    "1. See all users."
+  };
+
+  rc = sqlite3_open(argv[1], &db);
+  if( rc ) {
+    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return(1);
+  }
+
+  while( !rc ) {
+    for (int i = 0; i != OPERATION_COUNT; i++) {
+      printf("%s\n", states[i]);
+    }
+    scanf("%d", &operation);
+    switch(operation) {
+      case 1:
+        rc = testSQL(db, zErrMsg);
+        break;
+    }
+
+    if( rc!=SQLITE_OK ) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    }
+  }
+  sqlite3_close(db);
   return 0;
 }
