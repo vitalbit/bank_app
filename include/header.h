2dcc8c624849e672d14f413dd934d9dad848eb04
PavelGilevich <pavel-gilevich@mail.ru>
2014-12-10 13:14:14 +0300

    Update header.h
    added "editClient" and "addAccountToClient" methods

--- include/header.h
+++ include/header.h
@@ -9,3 +9,7 @@ int isAccountBlock(sqlite3 *db, int account_id);
 int credit(sqlite3 *db);
 
 int getAccountInfoById(sqlite3 *db, char *errmsg);
+
+void editClient(sqlite3 *db, int client_id, int field_num);
+
+void addAccountToClient(sqlite3 *db, int client_id, int acc_type_id);
