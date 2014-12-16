ab5110294485b9ce8efe8ecc120381ee859e373e
Zaharova Anna <opoccymkresh@gmail.com>
2014-12-17 01:01:29 +0300

    check account

--- include/accountOperation.c
+++ include/accountOperation.c
@@ -5,20 +5,61 @@
 
 #include "header.h"
 
+bool checkBlocking(sqlite3 *db, int account_id){
+	sqlite3_stmt *stmt = NULL;
+	char *checkBlocking = "Select is_block from Account WHERE account_id = ?";
+	sqlite3_prepare_v2(db, checkBlocking, strlen(checkBlocking), &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, account_id);
+	sqlite3_step(stmt);
+	if(sqlite3_column_int(stmt, 0) == 1)
+		return true;
+	else return false;
+}
+
+bool checkExistAccount(sqlite3 *db, int account_id){
+	sqlite3_stmt *stmt = NULL;
+	int result = 0;
+	char *checkBlocking = "Select count(*) from Account WHERE account_id = ?";
+	sqlite3_prepare_v2(db, checkBlocking, strlen(checkBlocking), &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, account_id);
+	sqlite3_step(stmt);
+	if(sqlite3_column_int(stmt, 0) == 1)
+		return true;
+	else return false;
+}
+
 void blockAccountByAccountID(sqlite3 *db, int account_id){
-  sqlite3_stmt *stmt = NULL;
-  char *sqlBlockAccountByAccountID = "UPDATE Account SET is_block = 1 WHERE account_id = ?";
-  sqlite3_prepare_v2(db, sqlBlockAccountByAccountID, strlen(sqlBlockAccountByAccountID), &stmt, NULL);
-  sqlite3_bind_int(stmt, 1, account_id);
-  sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
+	sqlite3_stmt *stmt = NULL;
+	char *sqlBlockAccountByAccountID = "UPDATE Account SET is_block = 1 WHERE account_id = ?";
+	if(!checkExistAccount(db,account_id)){
+		printf("Check account id!\n");
+		return;
+	}
+	sqlite3_prepare_v2(db, sqlBlockAccountByAccountID, strlen(sqlBlockAccountByAccountID), &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, account_id);
+	sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+	if(checkBlocking(db, account_id)){
+		printf("The account is blocked!\n");
+	}else{
+		printf("The account is not blocked!\n");
+	}
 }
 
 void unblockAccountByAccountID(sqlite3 *db, int account_id){
-  sqlite3_stmt *stmt = NULL;
-  char *sqlBlockAccountByAccountID = "UPDATE Account SET is_block = 0 WHERE account_id = ?";
-  sqlite3_prepare_v2(db, sqlBlockAccountByAccountID, strlen(sqlBlockAccountByAccountID), &stmt, NULL);
-  sqlite3_bind_int(stmt, 1, account_id);
-  sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
+	sqlite3_stmt *stmt = NULL;
+	char *sqlBlockAccountByAccountID = "UPDATE Account SET is_block = 0 WHERE account_id = ?";
+	if(!checkExistAccount(db,account_id)){
+		printf("Check account id!\n");
+		return;
+	}
+	sqlite3_prepare_v2(db, sqlBlockAccountByAccountID, strlen(sqlBlockAccountByAccountID), &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, account_id);
+	sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+	if(checkBlocking(db, account_id)==false){
+		printf("The account is unblocked!\n");
+	}else{
+		printf("The account is not unblocked!\n");
+	}
 }
