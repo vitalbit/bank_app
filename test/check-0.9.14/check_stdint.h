546662c96588d4eeba8a04dc6494d0d55ec73ac4
IFours <ilia.sachok@gmail.com>
2014-12-16 23:32:30 +0300

    update

--- test/check-0.9.14/check_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CHECK_CHECK_STDINT_H
-#define _CHECK_CHECK_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "check 0.9.14"
-/* generated using gnu compiler Apple LLVM version 6.0 (clang-600.0.56) (based on LLVM 3.5svn) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
