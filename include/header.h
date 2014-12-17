489ee4adc7408520de13af0fe3134ef59b019af8
vitalbit <vitalbit@tut.by>
2014-12-17 15:56:57 +0300

    Update header.h

--- include/header.h
+++ include/header.h
@@ -22,3 +22,6 @@ void deleteAccountByClientId(sqlite3 *db, int client_id);
 
 void checkBlockOnClient(sqlite3 *db, int client_id);
 
+void printAccountBalance(sqlite3 *db, int account_id);
+
+int createNewClient(sqlite3 *db, char *full_name, char *email, char *nickname, char *password);
